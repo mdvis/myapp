966796b0235ec68a8c60e717805de137a060cd8a
dewei.ma <xnmageis@outlook.com>
2024-08-04 11:41:49 +0800

    feat: web

---
+++ goapp/web/login.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "hello astaxie")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ goapp/web/login.gtpl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <form action="http://127.0.0.1:8090/login" method="post">
+        username: <input type="text" name="username">
+        password: <input type="password" name="password">
+        <input type="submit" value="登录">
+    </form>
+</body>
+</html>
