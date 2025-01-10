55d66673661f9b802df302c1c52c3038cc03a794
dewei.ma <xnmageis@outlook.com>
2025-01-10 11:02:17 +0800

    feat: plugin

---
+++ plug/background.js
@@ -0,0 +1,11 @@
+/**
+ * name: background.js
+ * author: Deve
+ * date: 2024-10-14
+ */
+
+console.log("----------------", chrome);
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("run run run");
+});
